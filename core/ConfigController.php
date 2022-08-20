0d302489c380d542630a16557f57def682e97b26
omartins-zs <gabrielmatheus209@gmail.com>
2022-08-19 22:46:36 -0300

    Init do projeto com PHPOO, padrão MVC e Composer

---
+++ core/ConfigController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Core;
+
+class ConfigController
+{
+
+    // private string $url;
+
+    public function __construct()
+    {
+        $this->url =  filter_input(INPUT_GET, "url", FILTER_DEFAULT);
+        echo "URL: {$this->url} <br>";
+    }
+}
