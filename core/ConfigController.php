53aa7645bca1a807b7cfd4cebabdc06e3cafb4bd
omartins-zs <gabrielmatheus209@gmail.com>
2022-08-23 23:11:11 -0300

    Limpando e Manipulando a URL

--- core/ConfigController.php
+++ core/ConfigController.php
@@ -6,10 +6,73 @@ class ConfigController
 {
 
     // private string $url;
+    // private array $urlConjunto;
+    // private string $urlController;
+    // private string $urlParametro;
+    // private string $urlSlugController;
+    // private array $format;
 
     public function __construct()
     {
-        $this->url =  filter_input(INPUT_GET, "url", FILTER_DEFAULT);
-        echo "URL: {$this->url} <br>";
+        if (!empty(filter_input(INPUT_GET, "url", FILTER_DEFAULT))) {
+            $this->url = filter_input(INPUT_GET, "url", FILTER_DEFAULT);
+            echo "URL: {$this->url} <br>";
+            echo "<br>";
+            $this->limparUrl();
+            echo "URL Limpa:  {$this->url} <br>";
+            echo "<br>";
+
+            $this->urlConjunto = explode("/", $this->url);
+            var_dump($this->urlConjunto);
+            echo "<br> <br>";
+            if (isset($this->urlConjunto[0])) {
+                $this->urlController = $this->slugController($this->urlConjunto[0]);
+            } else {
+                $this->urlController = "Home";
+            }
+            if (isset($this->urlConjunto[1])) {
+                $this->urlParamentro = $this->urlConjunto[1];
+            } else {
+                $this->urlParamentro = "";
+            }
+        } else {
+            $this->urlController = "Home";
+            $this->urlParamentro = "";
+        }
+
+        echo "Controller: {$this->urlController} - Parametro: {$this->urlParamentro}<br>";
+        echo "<br>";
+    }
+
+    private function limparUrl()
+    {
+        // Eliminar as tags
+        $this->url = strip_tags($this->url);
+        // Eliminar os espaços em branco
+        $this->url = trim($this->url);
+        // Eliminar a Barra no final da URL
+        $this->url = rtrim($this->url, "/");
+
+        // Exemplo: na url escrito como-criar-função
+
+        $this->format['a'] = 'ÀÁÂÃÄÅÆÇÈÉÊËÌÍÎÏÐÑÒÓÔÕÖØÙÚÛÜüÝÞßàáâãäåæçèéêëìíîïðñòóôõöøùúûýýþÿRr"!@#$%&*()_-+={[}]?;:.,\\\'<>°ºª ';
+        $this->format['b'] = 'aaaaaaaceeeeiiiidnoooooouuuuuybsaaaaaaaceeeeiiiidnoooooouuuyybyRr--------------------------------';
+
+        $this->url = strtr(utf8_decode($this->url), utf8_decode($this->format['a']), $this->format['b']);
+    }
+
+    private function slugController($slugController)
+    {
+        // Exemplo na url escrito: sobre-Empresa
+
+        //Converter para minusculo
+        $this->urlSlugController = strtolower($slugController);
+        //Converter o traço para espaço em branco
+        $this->urlSlugController = str_replace("-", " ", $this->urlSlugController);
+        //Converter a primeira letra de cada palavra para maiusculo
+        $this->urlSlugController = ucwords($this->urlSlugController);
+        //Retirar o espaço em braco
+        $this->urlSlugController = str_replace(" ", "", $this->urlSlugController);
+        return $this->urlSlugController;
     }
 }
