993225a3036a7c2334e977d9e47dcdce7339caf4
omartins-zs <gabrielmatheus209@gmail.com>
2022-10-14 22:45:10 -0300

    Criando template de views para melhor organizaçao

---
+++ app/sts/Views/include/cabecalho.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\sts\Controllers;
+
+// Exemplo antiga versão nao deu certo
+if (!defined('URL')) {
+    // Redirecionando para a Raiz do projeto
+    header("Location: /");
+    die("Erro: Página não encontrada!");
+}
+?>
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Celke</title>
+</head>
+
+<body>
---
+++ app/sts/Views/include/rodape.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\sts\Controllers;
+
+// Exemplo antiga versão nao deu certo
+if (!defined('URL')) {
+    // Redirecionando para a Raiz do projeto
+    header("Location: /");
+    die("Erro: Página não encontrada!");
+}
+?>
+
+</body>
+</html>
