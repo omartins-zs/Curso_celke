f923f03401b2ed26e8d01c90bdd64cacc2f5a4bc
omartins-zs <gabrielmatheus209@gmail.com>
2022-11-27 22:33:42 -0300

    Trazendo o conteudo dinâmico de serviços na home

--- app/sts/Models/StsHome.php
+++ app/sts/Models/StsHome.php
@@ -17,6 +17,10 @@ class StsHome
 {
     /** @var array $data Recebe os registros do banco de dados */
     private array $data;
+    /** @var array $dataTop Recebe os registros do banco de dados relacionado ao topo */
+    private array $dataTop;
+    /** @var array $dataServ Recebe os registros do banco de dados relacionado aos serviços */
+    private array $dataServ;
 
     /**
      * Instancia a classe genérica no helper responsável em buscar os registro no banco de dados.
@@ -25,11 +29,28 @@ class StsHome
      */
     public function index(): array
     {
+        $this->viewTop();
+        $this->viewServ();
+        return $this->data;
+    }
+
+    private function viewTop()
+    {
         $viewHome = new \App\sts\Models\helper\StsRead();
         $viewHome->fullRead("SELECT id, title_top, description_top, link_btn_top, txt_btn_top, image
-              FROM sts_homes_tops
-              LIMIT :limit", "limit=1");
-        $this->data = $viewHome->getResult();
-        return $this->data[0];
+                    FROM sts_homes_tops
+                    LIMIT :limit", "limit=1");
+        $this->dataTop = $viewHome->getResult();
+        $this->data['top'] = $this->dataTop[0];
+    }
+
+    private function viewServ()
+    {
+        $viewServ = new \App\sts\Models\helper\StsRead();
+        $viewServ->fullRead("SELECT id, title_serv, description_serv, icone_um_serv, titulo_um_serv, description_um_serv, icone_dois_serv, titulo_dois_serv, description_dois_serv, icone_tres_serv, titulo_tres_serv, description_tres_serv
+                    FROM sts_homes_servs
+                    LIMIT :limit", "limit=1");
+        $this->dataServ = $viewServ->getResult();
+        $this->data['serv'] = $this->dataServ[0];
     }
 }
--- app/sts/Views/home/home.php
+++ app/sts/Views/home/home.php
@@ -8,45 +8,48 @@ if (!defined('48b5ts')) {
 
 //Ler o registro da página home retornado do banco de dados
 //A função extract é utilizado para extrair o array e imprimir através do nome da chave
-extract($this->dados['sts_homes']);
+extract($this->dados['sts_homes']['top']);
 
 $imagem_topo = URL . "app/sts/assets/images/home_topo/" . $image;
 ?>
 
-<div class="jumbotron descr-top" style="background-image: url('<?php echo $imagem_topo; ?>');">
+<div class="jumbotron descr-top" style="background-image: url('<?= $imagem_topo; ?>');">
     <div class="container text-center">
-        <h1 class="display-4"><?php echo $title_top; ?></h1>
-        <p class="lead"><?php echo $description_top; ?></p>
-        <a class="btn btn-primary btn-lg" href="<?php echo $link_btn_top; ?>" role="button"><?php echo $txt_btn_top; ?></a>
+        <h1 class="display-4"><?= $title_top; ?></h1>
+        <p class="lead"><?= $description_top; ?></p>
+        <a class="btn btn-primary btn-lg" href="<?= $link_btn_top; ?>" role="button"><?= $txt_btn_top; ?></a>
     </div>
 </div>
 
+<?php
+extract($this->dados['sts_homes']['serv']);
+?>
 <div class="jumbotron descr-serv">
     <div class="container text-center">
-        <h2 class="display-4">Serviços</h2>
-        <p class="lead pb-4">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
+        <h2 class="display-4"><?= $title_serv; ?></h2>
+        <p class="lead pb-4"><?= $description_serv; ?></p>
 
         <div class="row">
             <div class="col-lg-4">
                 <div class="rounded-circle circulo centralizar border border-info shadow">
-                    <i class="fas fa-ship"></i>
+                    <i class="<?= $icone_um_serv; ?>"></i>
                 </div>
-                <h2 class="mt-4 mb-4">Serviço um</h2>
-                <p>Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Curabitur non ultricies mi, sit amet laoreet diam.</p>
+                <h2 class="mt-4 mb-4"><?= $titulo_um_serv; ?></h2>
+                <p><?= $description_um_serv; ?></p>
             </div>
             <div class="col-lg-4">
                 <div class="rounded-circle circulo centralizar border border-info shadow">
-                    <i class="fas fa-map-marked-alt"></i>
+                    <i class="<?= $icone_dois_serv; ?>"></i>
                 </div>
-                <h2 class="mt-4 mb-4">Serviço dois</h2>
-                <p>Quisque ut interdum nunc. Pellentesque metus neque, convallis sed vestibulum eu, viverra et justo. In laoreet diam nec nisl consectetur auctor.</p>
+                <h2 class="mt-4 mb-4"><?= $titulo_dois_serv; ?></h2>
+                <p><?= $description_dois_serv; ?></p>
             </div>
             <div class="col-lg-4">
                 <div class="rounded-circle circulo centralizar border border-info shadow">
-                    <i class="fas fa-snowplow"></i>
+                    <i class="<?= $icone_tres_serv; ?>"></i>
                 </div>
-                <h2 class="mt-4 mb-4">Serviço três</h2>
-                <p>Donec porttitor metus a arcu pulvinar ultricies. Aliquam commodo fermentum sapien quis porta. Nunc ac hendrerit libero, vel aliquam mauris.</p>
+                <h2 class="mt-4 mb-4"><?= $titulo_tres_serv; ?></h2>
+                <p><?= $description_tres_serv; ?></p>
             </div>
         </div>
     </div>
